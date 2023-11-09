f31bea30531ca048dc640d4ea039a1dbfa9b19e0
Tamav <tamav.peinetti@gmail.com>
2023-11-09 17:44:19 -0300

    Agregando forms y animaciones

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 let pathPublic = path.resolve("public");
 app.use(express.static(pathPublic));
 
+
 app.listen(3000, ()=>{  
     console.log("Servidor levantado")
 });
@@ -13,3 +14,20 @@ app.get("/", (req, res)=>{
     let homeHtml = path.resolve(__dirname, "./views/home.html")
     res.sendFile(homeHtml);
 });
+
+// app.get("/register", (req, res)=>{
+//     let registerHtml = path.resolve(__dirname, "./views/register.html")
+//     res.sendFile(registerHtml);
+// });
+
+app.get("/register", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+
+
+})
+
+app.get("/login", (req, res) =>{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+    
+
+})
