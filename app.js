f3ba005e507a545027d6be797cc5efde858ca88d
Tamav <tamav.peinetti@gmail.com>
2023-11-14 21:26:55 -0300

    Proyecto MercadoLiebre

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 let pathPublic = path.resolve("public");
 app.use(express.static(pathPublic));
 
-
-app.listen(3000, ()=>{  
-    console.log("Servidor levantado")
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=>{  
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
 app.get("/", (req, res)=>{
@@ -15,10 +15,7 @@ app.get("/", (req, res)=>{
     res.sendFile(homeHtml);
 });
 
-// app.get("/register", (req, res)=>{
-//     let registerHtml = path.resolve(__dirname, "./views/register.html")
-//     res.sendFile(registerHtml);
-// });
+
 
 app.get("/register", (req, res) =>{
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
